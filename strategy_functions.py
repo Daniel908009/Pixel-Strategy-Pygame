11f62ac13f70560e011943c6c6d796952ce4e0f2
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-06-28 10:30:55 +0100

    Functions moved to separate file, currently not working...

--- strategy_functions.py
+++ strategy_functions.py
@@ -2,7 +2,12 @@
 
 #necessary imports
 import random
+import time
+import pygame
 
+# global variables
+players_actual = []
+wars = []
 
 # Function to check if a player has some border tiles with another player and returning them
 def border_tiles_func(players, player1, player2):
@@ -44,10 +49,12 @@ def battle_logic_core(power1, power2, player1, player2):
     else:
         return player1
     
+
+
 # Function to change the owner of the tile after the battle
 def change_of_owner(winner_of_battle, tile_attacked, players, player1, player2):
     ply = players.copy()
-
+    print("change is about to happen")
     if winner_of_battle == player1:
         # Remove tile from player2's coordinates
         for i in range(ply[player2]["num_of_tiles"]):
@@ -85,3 +92,122 @@ def change_of_owner(winner_of_battle, tile_attacked, players, player1, player2):
             pass
 
     return ply
+
+# Map logic function, manages the diplomacy and battle logic
+def map_logic(running, game_speed, num_players):
+    global wars
+    while running:
+        diplomacy_logic(num_players)
+        battle_logic()
+        time.sleep(game_speed)
+
+
+# diplomacy logic function, decides what will happen between players
+def diplomacy_logic(num_players):
+    global wars
+    chance_of_nothing = 5
+    diplomacy_options = []
+    chance_of_peace = 1
+    chance_of_war = 3
+    for i in range(chance_of_nothing):
+        diplomacy_options.append("nothing")
+
+    for j in range(chance_of_peace):
+        diplomacy_options.append("peace")
+    
+    for k in range(chance_of_war):
+        diplomacy_options.append("war")
+    
+    for i in range(num_players):
+        choice = random.choice(diplomacy_options)
+        if choice == "peace":
+            peace_logic(i)
+        elif choice == "war":
+            war_logic(i, num_players)
+        else:
+            pass
+
+# war logic function, decides how will war be implemented between 2 players
+def war_logic(player, num_players):
+    global wars
+    enemy = random.randint(0, num_players-1)
+    if enemy == player or (player, enemy) in wars or (enemy, player) in wars:
+        pass
+    else:
+        wars.append((player, enemy))
+
+# peace logic function, removes players from wars list, currently peaces out with all other players 
+# eventualy only one war will be peaced out, working on it
+def peace_logic(player):
+    global wars
+    possible_peace = []
+    for i in range(len(wars)):
+        if wars[i][0] == player or wars[i][1] == player:
+            possible_peace.append(i)
+    m = 0
+    for i in possible_peace:
+        wars.pop(i-m)
+        m += 1
+
+def battle_logic():
+    global players_actual, wars
+    if len(wars) == 0:
+        pass
+    else:
+        for i in range(len(wars)):
+            player1 = wars[i][0]
+            player2 = wars[i][1]
+            print("battle is about to happen")
+            print(player1, player2)
+            print(players_actual[player1]["num_of_tiles"], players_actual[player2]["num_of_tiles"])
+
+            power1 = players_actual[player1]["num_of_tiles"]
+            power2 = players_actual[player2]["num_of_tiles"]
+            tile_attacked = []
+            border_tiles = []
+            winner_of_battle = 0
+            border_tiles.append(border_tiles_func(players_actual, player1, player2))
+            try:
+                tile_attacked.append(random.choice(border_tiles[0]))
+                winner_of_battle= battle_logic_core(power1, power2, player1, player2)
+                players_actual = change_of_owner(winner_of_battle, tile_attacked[0], players_actual, player1, player2)
+                border_tiles.clear()
+                tile_attacked.clear()
+            except IndexError:
+                pass
+           
+# removing occupied tiles from free tiles
+def remove_occupied_tiles(players, num_players, map_free_tiles, map_occupied_tiles, index_version_of_free_tiles):
+    map_free_tiles -= num_players
+    for i in range(num_players):
+        map_occupied_tiles.append(players[i]["coordinates"][0])
+        try:
+            index_version_of_free_tiles.remove(players[i]["coordinates"][0])
+        except ValueError:
+            pass
+    return map_free_tiles
+
+# expansion logic function, finds free tiles around all the player controled ones, currently only in 4 directions, will be expanded to 8 once the game is more advanced
+def expandsion_initial(num_players,map_free_tiles, map_occupied_tiles, index_version_of_free_tiles):
+    potential_tiles = []
+    free_potential_tiles = []
+    for i in range(num_players):
+        for j in range(len(players_actual[i]["coordinates"])):
+            potential_tiles.append((players_actual[i]["coordinates"][j][0]+1, players_actual[i]["coordinates"][j][1]))
+            potential_tiles.append((players_actual[i]["coordinates"][j][0]-1, players_actual[i]["coordinates"][j][1]))
+            potential_tiles.append((players_actual[i]["coordinates"][j][0], players_actual[i]["coordinates"][j][1]+1))
+            potential_tiles.append((players_actual[i]["coordinates"][j][0], players_actual[i]["coordinates"][j][1]-1))
+        list1 = potential_tiles
+        list2 = index_version_of_free_tiles
+        free_potential_tiles = [value for value in list1 if value in list2]
+        try:
+            random_tile = random.choice(free_potential_tiles)
+            index_version_of_free_tiles.remove(random_tile)
+            map_free_tiles -= 1
+            players_actual[i]["coordinates"].append(random_tile)
+            players_actual[i]["num_of_tiles"] += 1
+            free_potential_tiles.clear()
+            potential_tiles.clear()
+        except IndexError:
+            pass
+    return map_free_tiles
