ef0c3169991f434af84c2784b009fd5e8169ee67
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-06-28 20:24:28 +0100

    Added a reset button, curently working only partialy as threading doesnt realy like this.

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import threading
 import pygame
 import random
 import time
-from strategy_functions import map_logic, remove_occupied_tiles, expandsion_initial, send_important_data, stop_all_functions
+from strategy_functions import map_logic, remove_occupied_tiles, expandsion_initial, send_important_data, stop_all_functions, start_all_functions
 
 pygame.init()
 
 # map logic and free tiles logic
-map_size = 20
+map_size = 30
 pixel_size = 20
 ratio = (1, 2)
 map = []
@@ -24,10 +24,10 @@ wars = []
 game_speed = 0.09
 
 # Screen setings
-screen = pygame.display.set_mode(((map_size*pixel_size+map_size*pixel_size/10)*ratio[1], (map_size*pixel_size)*ratio[0]))
+screen = pygame.display.set_mode(((map_size*pixel_size+map_size*pixel_size/3)*ratio[1], (map_size*pixel_size)*ratio[0]))
 pygame.display.set_caption("Pixel Strategy")
 pygame.display.set_icon(pygame.image.load("Pixel_strategy/strategy.png"))
-controlwindowsize = (map_size*pixel_size/10)*ratio[1]
+controlwindowsize = (map_size*pixel_size/3)*ratio[1]
 
 # creating map and free tiles
 map_free_tiles = (map_size*ratio[0])*(map_size*ratio[1])
@@ -41,7 +41,7 @@ for i in range(map_size*ratio[1]):
 # creating players and setting their initial position
 players = []
 players_actual = []
-num_players = 10
+num_players = 20
 for i in range(num_players):
     playercoords1 = (random.randint(0, (map_size-1)*ratio[1]), random.randint(0, (map_size-1)*ratio[0]))
     map_occupied_tiles.append((playercoords1 ))   
@@ -53,7 +53,7 @@ for i in range(num_players):
         }
     )
     
-# drawing players and player controlled tiles, this cant be done in the functions file as pygame doesnt support this
+# drawing players and player controlled tiles, this cant be done in the functions file as pygame doesnt support this kind of thing
 def drawing_players():
     global running, num_players, players, screen, pixel_size
     players_actual = players
@@ -64,9 +64,39 @@ def drawing_players():
                     pygame.draw.rect(screen, players_actual[i]["color"], (players_actual[i]["coordinates"][j][0]*pixel_size, players_actual[i]["coordinates"][j][1]*pixel_size, pixel_size, pixel_size))
                 except IndexError:
                     pass
-
+  
+# function for reseting the game and setting the initial values again, currently working only partially, threading has some issues with this part
 def reset_game():
-    pass
+    print("reseting game")
+    for i in range(num_players):
+        playercoords1 = (random.randint(0, (map_size-1)*ratio[1]), random.randint(0, (map_size-1)*ratio[0]))
+        map_occupied_tiles.append((playercoords1 ))   
+        players[i] = {
+            "color": (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+            "coordinates": [(playercoords1[0], playercoords1[1])],
+            "num_of_tiles": 1
+        }
+    global map_free_tiles, index_version_of_free_tiles, initial_check, just_once, Threads_started, sended, running
+    map_free_tiles = (map_size*ratio[0])*(map_size*ratio[1])
+    index_version_of_free_tiles.clear()
+    for i in range(map_size*ratio[1]):
+        for j in range(map_size*ratio[0]):
+            index_version_of_free_tiles.append((i, j))
+    initial_check = True
+    just_once = True
+    Threads_started = False
+    sended = False
+    running = False
+    stop_all_functions()
+    try:
+        thread1.join()
+        thread2.join()
+    except RuntimeError:
+        pass
+    time.sleep(0.1)
+    running = True
+    start_all_functions()
+    print("game reseted")
 
 
 running = True
@@ -87,6 +117,14 @@ while running:
 
     #control screen color and size
     pygame.draw.rect(screen, (50, 50, 50), ((map_size*pixel_size)*ratio[1], 0, controlwindowsize, (map_size*pixel_size)*ratio[0] ))
+    #buttons inside control screen
+    pygame.draw.rect(screen, (0, 0, 0), ((map_size*pixel_size)*ratio[1]+controlwindowsize/50, (map_size*pixel_size)*ratio[0]/20, controlwindowsize-controlwindowsize/20, (map_size*pixel_size)*ratio[0]/5))
+    pygame.draw.rect(screen, (0, 0, 0), ((map_size*pixel_size)*ratio[1]+controlwindowsize/50, (map_size*pixel_size)*ratio[0]/2, controlwindowsize-controlwindowsize/20, (map_size*pixel_size)*ratio[0]/12))
+
+    #text inside buttons
+    font = pygame.font.Font(None, int((map_size*pixel_size)*ratio[0]/12))
+    text = font.render("Reset", True, (255, 255, 255))
+    screen.blit(text, ((map_size*pixel_size)*ratio[1]+controlwindowsize/2.5, (map_size*pixel_size)*ratio[0]/2))
 
     #event checking
     for event in pygame.event.get():
@@ -94,8 +132,15 @@ while running:
             stop_all_functions()
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.type == pygame.r:
+            if event.key == pygame.K_ESCAPE:
+                stop_all_functions()
+                running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse = pygame.mouse.get_pos()
+            if(map_size*pixel_size)*ratio[1]+controlwindowsize/50 <= mouse[0] <= (map_size*pixel_size)*ratio[1]+controlwindowsize-controlwindowsize/20 and (map_size*pixel_size)*ratio[0]/2 <= mouse[1] <= (map_size*pixel_size)*ratio[0]/2+(map_size*pixel_size)*ratio[0]/12:
                 reset_game()
+            elif (map_size*pixel_size)*ratio[1]+controlwindowsize/50 <= mouse[0] <= (map_size*pixel_size)*ratio[1]+controlwindowsize-controlwindowsize/20 and (map_size*pixel_size)*ratio[0]/20 <= mouse[1] <= (map_size*pixel_size)*ratio[0]/20+(map_size*pixel_size)*ratio[0]/5:
+                pass
     
     # checking if players are not on top of each other
     while initial_check:
